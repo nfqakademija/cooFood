599460c617683ca45dbf05ef79b6593ff5da4a22
tadas.siaudvytis@gmail.com <tadas.siaudvytis@gmail.com>
2015-11-15 14:53:09 +0200

    finally renamed

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,6 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use cooFood\eventBundle\Controller\eventController;
 
 class DefaultController extends Controller
 {
