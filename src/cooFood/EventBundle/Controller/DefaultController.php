70eb46cfc877c98b462121186761060c574aa572
rokasmi <mikalkenas@gmail.com>
2015-11-15 19:47:30 +0200

    Deprecate fix 3

--- src/cooFood/EventBundle/Controller/DefaultController.php
+++ src/cooFood/EventBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace cooFood\EventBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
